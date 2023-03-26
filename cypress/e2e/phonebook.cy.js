414bc17176eab2776b1ccd483010101416a52fa3
Marco Tidu <tidumarco@gmail.com>
2023-03-26 22:20:31 +0300

    Added testing in pipeline

---
+++ cypress/e2e/phonebook.cy.js
@@ -0,0 +1,26 @@
+describe("phonebook tests", () => {
+  it("front page can be opened", () => {
+    cy.visit("http://localhost:8080/");
+    cy.contains("Phonebook");
+    cy.contains("Add a new person");
+    cy.contains("Search a person");
+    cy.contains("Numbers");
+  });
+  it("add new person form can be filled", function () {
+    cy.visit("http://localhost:8080");
+    cy.contains("name")
+      .closest("div") // navigate up to the closest parent div
+      .find("input")
+      .type("Giorgio Gustavi");
+    cy.contains("number")
+      .closest("div") // navigate up to the closest parent div
+      .find("input")
+      .type("0185-333121");
+    cy.contains("add").click();
+  });
+  it("a person can be searched", function () {
+    cy.visit("http://localhost:8080");
+    cy.get("input:first").type("Giorgio Gustavi");
+    cy.contains("Giorgio Gustavi");
+  });
+});
